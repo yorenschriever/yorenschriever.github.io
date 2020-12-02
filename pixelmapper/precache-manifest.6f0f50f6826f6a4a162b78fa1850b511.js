9f78835350316310146fb30b08be3fa7d07b406e
Yoren Schriever <yoren@ocelot.nl>
2020-12-02 09:07:18 +0100

    pixelmapper

---
+++ pixelmapper/precache-manifest.6f0f50f6826f6a4a162b78fa1850b511.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf3cd5ba5c31f0c00a6c2b0e9115ebd",
+    "url": "/pixelmapper/index.html"
+  },
+  {
+    "revision": "7b0df5da4afb69ab9e87",
+    "url": "/pixelmapper/static/css/main.488477fe.chunk.css"
+  },
+  {
+    "revision": "08383971382b025d625379542212747f",
+    "url": "/pixelmapper/static/js/0.d257af3b.chunk.worker.js"
+  },
+  {
+    "revision": "906c8a35cf58ebf144e815a563375e87",
+    "url": "/pixelmapper/static/js/0.d257af3b.chunk.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "9954464fdd2dfe040785",
+    "url": "/pixelmapper/static/js/2.bf544bb6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pixelmapper/static/js/2.bf544bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0df5da4afb69ab9e87",
+    "url": "/pixelmapper/static/js/main.d6bdaf0c.chunk.js"
+  },
+  {
+    "revision": "aa7b73fad63e6934def1",
+    "url": "/pixelmapper/static/js/runtime-main.7f018c13.js"
+  },
+  {
+    "revision": "41a8ee217f520177be0d71936dd1970e",
+    "url": "/pixelmapper/static/media/code0.41a8ee21.png"
+  },
+  {
+    "revision": "34bf7ea2e1c4b10c3d2f46f7759e2060",
+    "url": "/pixelmapper/static/media/code1.34bf7ea2.png"
+  },
+  {
+    "revision": "5a3b9220d772aa7c254015296da87dfb",
+    "url": "/pixelmapper/static/media/result.5a3b9220.png"
+  },
+  {
+    "revision": "964842f074722fece981c26547571fcb",
+    "url": "/pixelmapper/static/media/white.964842f0.png"
+  }
+]);
